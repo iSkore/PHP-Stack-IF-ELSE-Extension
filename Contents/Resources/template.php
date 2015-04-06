5320a2bf94ec3bb9c28efa16e3c0b13e31866c7b
iSkore Development Inc <development@dreamthegame.com>
2015-04-06 15:29:14 -0400

    Comment code and add information file

--- Contents/Resources/template.php
+++ Contents/Resources/template.php
@@ -1,14 +1,28 @@
+<!-- Create header for display in editor -->
 %[if edit]% <h1 class="headIf">IF : </h1> %[endif]%
+
+<!-- Create collapsable div for organization in editor -->
 %[if edit]% <div class="col"> %[endif]%
+
+<!-- Start PHP -->
 <?php
+    
+    // Start if statement and put corresponding ID from UI inside (...)
     if (%id=phpCode_If%) {
         ?>
+            <!-- Drop in a stack -->
             %slice%
         <?php
+        // Start else if statement and put corresponding statement from UI inside
     } else if (%id=phpCode_Else_1%) {
+        
+        // Check if the user enabled the use of this element
         %[if %("%id=else1Bool%" == "true")%]%
             ?>
+                <!-- Put in a header for this ELSE IF -->
                 %[if edit]%<h1>ELSE IF : </h1>%[endif]%
+                
+                <!-- Drop in a stack -->
                 %slice%
             <?php
         %[endif]%
@@ -42,4 +56,9 @@
         %[endif]%
     }
 ?>
+<!-- End PHP -->
+
+<!-- Close collapsable div for organization in editor -->
 %[if edit]% </div> %[endif]%
+
+<!-- See --><a>https://yourhead.tenderapp.com/kb/stacks-api/</a><!-- for more information about developing stacks for RapidWeaver -->
